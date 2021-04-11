17f79264c4d3fc1f2fc53a8c539fafbce8756a38
akash chhetri <akash.chhetri1@gmail.com>
2021-04-11 11:53:59 +1000

    ball hitting the user and computer paddle, reflecting

--- js/initGame.js
+++ js/initGame.js
@@ -1,11 +1,16 @@
 const initGame = () => {
+  const wallBorderGap = 4;
+  const paddleHeight = 100;
   var canvas;
   var canvasContext;
   var ballX = 15;
   var ballY = 15;
   var ballSpeedX = 5;
   var ballSpeedY = 5;
-  var wallBorderGap = 4;
+  var paddleWidth = 10;
+  var userPaddle = 0;
+  var computerPaddle = 0;
+
   window.onload = function () {
     canvas = document.getElementById("gameCanvas");
     canvasContext = canvas.getContext("2d");
@@ -14,14 +19,41 @@ const initGame = () => {
       moveGameBall();
       drawGameEverything();
     }, 1000 / framesPerSecond);
+    canvas.addEventListener("mousemove", function (evt) {
+      var mousePos = calculateMousePosition(evt);
+      userPaddle = mousePos.y - paddleHeight / 2;
+      computerPaddle = mousePos.y - paddleHeight / 2;
+    });
   };
+
+  function ballReset() {
+    ballSpeedX *= -1;
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
+  }
+
+  function calculateMousePosition(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+      x: mouseX,
+      y: mouseY,
+    };
+  }
 
   function drawGameEverything() {
     colorCanvasObject(0, 0, canvas.width, canvas.height, "black"); // black screen
-    // colorCanvasObject(canvas.width / 2, canvas.height / 2, 1, 5, "white"); // game screen divider
-    colorCanvasObject(0, 0, 10, 100, "white"); // user game paddle
-    colorCanvasObject(canvas.width - 4, canvas.height - 25, 4, 25, "white"); // user game paddle
-    gameBall();
+    colorCanvasObject(0, userPaddle, paddleWidth, paddleHeight, "white"); // left user game paddle
+    colorCanvasObject(
+      canvas.width - paddleWidth,
+      computerPaddle,
+      paddleWidth,
+      paddleHeight,
+      "white"
+    ); // right computer game paddle
+    gameBall(ballX, ballY, 10, "yellow");
   }
 
   function colorCanvasObject(leftX, topY, width, height, objectColor) {
@@ -29,10 +61,10 @@ const initGame = () => {
     canvasContext.fillRect(leftX, topY, width, height);
   }
 
-  function gameBall() {
-    canvasContext.fillStyle = "yellow";
+  function gameBall(centerX, centerY, radius, ballColor) {
+    canvasContext.fillStyle = ballColor;
     canvasContext.beginPath();
-    canvasContext.arc(ballX, ballY, 8, 0, Math.PI * 2, true);
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
     canvasContext.fill();
   }
 
@@ -43,8 +75,18 @@ const initGame = () => {
   }
 
   function changeDirection() {
-    if (ballX > canvas.width - wallBorderGap || ballX < wallBorderGap) {
-      ballSpeedX *= -1;
+    if (ballX < wallBorderGap) {
+      if (ballY > userPaddle && ballY < userPaddle + paddleHeight) {
+        ballSpeedX *= -1;
+      } else {
+        ballReset();
+      }
+    } else if (ballX > canvas.width - wallBorderGap) {
+      if (ballY > computerPaddle && ballY < computerPaddle + paddleHeight) {
+        ballSpeedX *= -1;
+      } else {
+        ballReset();
+      }
     }
     if (ballY > canvas.height - wallBorderGap || ballY < wallBorderGap) {
       ballSpeedY *= -1;
