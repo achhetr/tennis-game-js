5a582b4f6ccef4e0593bb15bdd9da55c269dd356
akash chhetri <akash.chhetri1@gmail.com>
2021-04-11 10:08:07 +1000

    game ball width and height change to appear like a square ball

--- js/initGame.js
+++ js/initGame.js
@@ -1,11 +1,16 @@
 const initGame = () => {
   var canvas;
   var canvasContext;
+  var ballX = 100;
+  var ballY = 100;
   window.onload = function () {
     canvas = document.getElementById("gameCanvas");
     canvasContext = canvas.getContext("2d");
     drawBlackBackground(canvasContext);
     drawGameDivider(canvasContext);
+    ballX = 100;
+    ballY = 100;
+    drawGameBall(canvasContext, ballX, ballY);
   };
 
   function drawBlackBackground(canvasContext) {
@@ -16,6 +21,13 @@ const initGame = () => {
   function drawGameDivider(canvasContext) {
     canvasContext.fillStyle = "white";
     canvasContext.fillRect(canvas.width / 2, canvas.height / 2, 1, 5);
+  }
+
+  function drawGameBall(canvasContext, ballX, ballY) {
+    ballX += Math.floor(Math.random() * 100);
+    ballY += Math.floor(Math.random() * 100);
+    canvasContext.fillStyle = "yellow";
+    canvasContext.fillRect(ballX, ballY, 4, 3);
   }
 };
 
